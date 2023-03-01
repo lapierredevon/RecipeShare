d0995fded964688873fe3616247dcd332ba1f0d1
Devon Lapierre <lapierredevon@gmail.com>
2023-03-01 06:05:35 -0500

    fix-layout-start

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './App'; 
+import {
+  BrowserRouter as Router
+} from 'react-router-dom';
 
 
 const root = ReactDOM.createRoot(
@@ -9,7 +12,9 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>
 );
 
